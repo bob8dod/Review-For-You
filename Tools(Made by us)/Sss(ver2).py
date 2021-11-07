eccd1a2bd24eb8777240304849f2526b6702909c
Seung-Il Park <72644713+bob8dod@users.noreply.github.com>
2021-11-07 23:16:10 +0900

    Update Sss(ver2).py

--- Tools(Made by us)/Sss(ver2).py
+++ Tools(Made by us)/Sss(ver2).py
@@ -61,3 +61,25 @@ if __name__ == '__main__':
     pool.join()
     print(len(text))
     print("실행 시간 : %s초" % (time.time() - start_time))
+    
+def sss(text):
+    text = re.sub(r" {2,}", " ", hangul.sub(' ', text))
+    end_char = ['요', '다', '죠']
+    avoid_char = ['보다', '하려다', '하다', '려다']
+    special_char = ['느림']
+    new_sentences = []
+    ts = text.split()
+    start, end, flag = 0, 0, 0
+    for i in range(len(ts)):
+        if (len(ts[i]) >= 2 and ts[i][-1] in end_char and ts[i][-2:] not in avoid_char)\
+                or ('ETN' in mecab.pos(ts[i])[-1][1]) or (ts[i][-2:] in special_char): # and mecab.pos(ts[i])[-1][1] != 'NNG'
+            end = i
+            new_sentences.append(' '.join(ts[start:end + 1]).strip())
+            start = end + 1
+            flag = 1
+        else:
+            if i == len(ts)-1:
+                new_sentences.append(' '.join(ts[start:]).strip())
+    if not flag and len(new_sentences) == 0:
+        new_sentences.append(text)
+    return new_sentences
